1d9d30827022988d117260dc636102d7c52de71b
Ashutosh <sahuashutosh1408@gmail.com>
2026-01-19 13:28:48 +0530

    Hyper Parameter Tuning

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -44,7 +44,38 @@ class ModelTrainer:
                 "CatBoost Regressor":CatBoostRegressor(verbose=False),
                 "AdaBoost Regressor":AdaBoostRegressor()
             }
-            model_report:dict=evaluate_models(X_train,y_train,X_test,y_test,models=models)
+            params={
+                "Decision Tree":{
+                    'criterion':['squared_error','friedman_mse','absolute_error','poisson'],
+                },
+                "Random Forest":{
+                    'n_estimators':[8,16,32,64,128,256]
+                },
+                "Gradient Boosting":{
+                    'learning_rate':[.1,.01,.05,.001],
+                    'subsample':[0.6,0.7,0.75,0.8,0.85,0.9],
+                    'n_estimators':[8,16,32,64,128,256]
+                },
+                "K-Neighbors Regressor":{
+                    'n_neighbors':[5,7,9,11],
+                    'weights':['uniform','distance']
+                },
+                "XGB Regressor":{
+                    'learning_rate':[.1,.01,.05,.001],
+                    'n_estimators':[8,16,32,64,128,256]
+                },
+                "CatBoost Regressor":{
+                    'depth':[6,8,10],
+                    'learning_rate':[.1,.01,.05,.001],
+                    'iterations':[30,50,100]
+                },
+                "AdaBoost Regressor":{
+                    'learning_rate':[.1,.01,.05,.001],
+                    'n_estimators':[8,16,32,64,128,256]
+                }
+                
+            }
+            model_report:dict=evaluate_models(X_train,y_train,X_test,y_test,models=models,param=params)
             
             best_model_score=max(sorted(model_report.values()))
             
--- src/utlis.py
+++ src/utlis.py
@@ -4,6 +4,7 @@ import pandas as pd
 import numpy as np
 import dill
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 from src.exception import CustomException
 
@@ -26,8 +27,14 @@ def evaluate_models(X_train,y_train,X_test,y_test,models):
         
         for i in range(len(models)):
             model=list(models.values())[i]
+            para=param[list(models.keys())[i]]
+            
+            gs=GridSearchCV(model,para,cv=3)
+            gs.fit(X_train,y_train)
+            
             
             # Train model
+            model.set_params(**gs.best_params_)
             model.fit(X_train,y_train)
             
             # Predicting the test set results
